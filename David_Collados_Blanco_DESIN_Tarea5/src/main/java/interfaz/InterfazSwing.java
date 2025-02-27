3ac11f5caf8d2c60c561df0ef250a00c7135b5aa
Deviitt11 <david.collados11@gmail.com>
2025-02-27 13:05:11 +0100

    Creación MavenProj - 27feb

---
+++ David_Collados_Blanco_DESIN_Tarea5/src/main/java/interfaz/InterfazSwing.java
@@ -0,0 +1,54 @@
+package interfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class InterfazSwing extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfazSwing frame = new InterfazSwing();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InterfazSwing() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnBoton = new JButton("Botón");
+		btnBoton.setBounds(169, 119, 89, 23);
+		contentPane.add(btnBoton);
+		
+		JLabel lblDesin = new JLabel("DESIN T5");
+		lblDesin.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblDesin.setBounds(172, 42, 109, 29);
+		contentPane.add(lblDesin);
+	}
+}
